c68a6e9ffe8f2f76789df373fc37387a1c1a3065
Stefan Ingolfsson <stefan.ingolfsson@gmail.com>
2022-03-10 22:45:32 +0000

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container, Header } from "semantic-ui-react";
+
+function App() {
+  return (
+    <Container>
+      <Header as="h1" content="page" />
+    </Container>
+  );
+}
+
+export default App;
